6fb79b46476e88551a7e0272d01bf3266078a6d3
mfadlihs <winnerfadli999@gmail.com>
2023-07-25 13:58:17 +0700

    init

---
+++ src/pages/style-guide.js
@@ -0,0 +1,15 @@
+export default function StyleGuide() {
+  return (
+    <div>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <div className="body">Body 1</div>
+      <div className="body2">Body 2</div>
+      <div>BUTTON</div>
+    </div>
+  );
+}
